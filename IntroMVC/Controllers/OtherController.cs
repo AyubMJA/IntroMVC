619d120190d97433fede47dd28b5de7bf8bdae95
Ayub Ali <ayubali8448@icloud.com>
2021-09-23 14:57:01 -0400

    added otherController learning how the viewBag object works.

---
+++ IntroMVC/Controllers/OtherController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntroMVC.Controllers
+{
+    public class OtherController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Post(string id)
+        {
+            ViewBag.id = id;
+            return View();
+        }
+    }
+}
